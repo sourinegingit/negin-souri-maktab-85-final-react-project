ec97692689b4de03a66e1121b7029f783cd42fc9
negin <nsouri7979@gmail.com>
2023-04-07 11:24:37 +0430

    category product

---
+++ src/components/productCategory/CategoryProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CategoryProduct = () => {
+    
+  return (
+    <div className='w-screen bg-red-300 mr-64'>
+      main 
+    </div>
+  )
+}
+
+export default CategoryProduct
