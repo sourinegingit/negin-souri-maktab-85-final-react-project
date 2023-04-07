4c838e98d92826c102d09182009a9c626451e277
negin <nsouri7979@gmail.com>
2023-04-07 11:25:15 +0430

    index

---
+++ src/components/productCategory/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Collection from "../collections";
+
+
+
+const ProductCategory = () => {
+  return (
+    <div className="w-screen justify-around flex">
+      <Collection/>
+    </div>
+  );
+};
+
+export default ProductCategory;
