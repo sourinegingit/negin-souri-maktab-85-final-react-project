516a124ac761d8a3c2bb6b1331476ebbddd2168e
negin <nsouri7979@gmail.com>
2023-04-07 10:52:33 +0430

    home page

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Carts from "../../components/carts";
+
+const Home = () => {
+  return (
+    <div>
+      <Carts />
+    </div>
+  );
+};
+
+export default Home;
