5b3c347b20c4852b0b659705ab934e03a452353d
negin <nsouri7979@gmail.com>
2023-04-07 11:17:34 +0430

    carts

---
+++ src/components/carts/index.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import Cart from "../cart";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchTodos } from "../../redux/feature/cart-slice";
+import {IoIosArrowBack} from "react-icons/io"
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Category from "../category";
+
+const Carts = () => {
+
+  const navigate=useNavigate()
+  const handleNavigate=(route)=>{
+    navigate(`/${route}`)
+  }
+  const { products } = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+   axios.get()
+  }, [dispatch]);
+
+
+  return (
+    <>
+    <Category id={3} navigationText='spingCollection' text='شال بهاره'/>
+    <Category id={2} navigationText='summerCollection' text='شال تابستانه'/>
+    <Category id={4} navigationText='fallCollection' text=' شال پاییزه'/>
+    <Category id={1} navigationText='winterCollection' text='شال زمستانه'/>
+    </>
+  );
+};
+
+export default Carts;
